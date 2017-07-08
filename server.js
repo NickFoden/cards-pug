edf03ee31dbe771489b23a74a724c4ed6c47c4d0
Nick Foden <nickfoden@gmail.com>
2017-07-08 14:42:18 -0400

    Added Css Stylesheet to pug layout

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use('/static', express.static('public'));
 
 app.get('/', function(req, res){
 	res.render('index')
