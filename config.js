faf937b626970a484a6f3d5596738fd593842009
Nick Foden <nickfoden@gmail.com>
2018-01-28 20:18:44 -0500

    Adding some css grid and changing colors.

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-						global.DATABASE_URL ||
-						'mongodb://localhost/cards-db';
+exports.DATABASE_URL =
+  // process.env.DATABASE_URL
+  "mongodb://cards-user:BlueCity25!@ds129031.mlab.com:29031/cards_js" ||
+  global.DATABASE_URL ||
+  "mongodb://localhost/cards-db";
 
-exports.TESTDATABASE_URL = process.env.TestDatabase_URL ||
-							'mongodb://localhost/test-cards-db';
+exports.TESTDATABASE_URL =
+  process.env.TestDatabase_URL || "mongodb://localhost/test-cards-db";
 
 exports.PORT = process.env.PORT || 8080;
