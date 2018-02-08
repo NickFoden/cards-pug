2a2833310d2a6c5dfd0e27fb0525e81a0ed23c36
Nick Foden <nickfoden@gmail.com>
2018-02-07 22:41:49 -0500

    Adding css variable changing some sizing and colors

--- public/cards.js
+++ public/cards.js
@@ -2,14 +2,11 @@ let used = [];
 let cards_js = [];
 
 function loadDb() {
-  $.getJSON(
-    "/cards",
-    function (result){
-      cards_js = result;
-      currentCard = getCard(cards_js);
-    }
-  )
-}  
+  $.getJSON("/cards", function(result) {
+    cards_js = result;
+    currentCard = getCard(cards_js);
+  });
+}
 
 loadDb();
 
@@ -22,113 +19,118 @@ let getCard = () => {
       notUsed = false;
     }
   }
-  if ((used.length - 1) === cards_js.length) {
+  if (used.length - 1 === cards_js.length) {
     return false;
   } else {
     return cards_js[index];
   }
 };
- 
-function displayCardQuestion(data){
-  let resultElement = 
-    `<div class="display-card-question">
+
+function displayCardQuestion(data) {
+  let resultElement = `<div class="display-card-question">
       <h2>${data.question}</h2>
-      </div><button id="answer-button">Flip It</button>`;
+      </div><button id="answer-button">Show Answer</button>`;
 
-  $('#display').html(resultElement);
-};
+  $("#display").html(resultElement);
+}
 
-function displayCardAnswer(data){
-  let resultElement = 
-    `<div class="display-card-answer">
-    <h2><a href="http://${data.reference}" target="_blank">${data.answer}</a></h2>
+function displayCardAnswer(data) {
+  let resultElement = `<div class="display-card-answer">
+    <h2><a href="http://${data.reference}" target="_blank">${
+    data.answer
+  }</a></h2>
     </div><button id="next-card">Next</button>`;
-    $('#answer-button').hide();
-    $('#display').append(resultElement);
-};
+  $("#answer-button").hide();
+  $("#display").append(resultElement);
+}
 
-function displayEnd(data){
+function displayEnd(data) {
   let resultElement = `<div class="end-card">You have reached the end</div>
                         <button id="start-over" onclick="location.href='start.html'">Start over</button>
                         <button id="index" onclick="location.href='summary'">Index of Cards</button>`;
 
-   $('#display').html(resultElement);  
-};
+  $("#display").html(resultElement);
+}
 
-function displaySummary(data){
-  let check = sessionStorage.getItem('user');
-  console.log('check ' + check)
-  let erase = "hidden"
-  if (check){
-    erase = ""
-  } 
+function displaySummary(data) {
+  let check = sessionStorage.getItem("user");
+  console.log("check " + check);
+  let erase = "hidden";
+  if (check) {
+    erase = "";
+  }
   console.log("erase " + erase);
-  for (i = 0; i < data.length; i++){
-    let summary = `<li><h5><a href="http://${data[i].reference}" target="_blank">${data[i].question}</a><button id="${data[i]._id}" class="delete ${erase}">Delete</button></h5></li>`;
-    $('.cards-summary').append(summary);
+  for (i = 0; i < data.length; i++) {
+    let summary = `<li><h5><a href="http://${
+      data[i].reference
+    }" target="_blank">${data[i].question}</a><button id="${
+      data[i]._id
+    }" class="delete ${erase}">Delete</button></h5></li>`;
+    $(".cards-summary").append(summary);
   }
 }
 
-function deleteCard(id){
+function deleteCard(id) {
   $.ajax({
     type: "DELETE",
     url: `/cards/${id}`,
     dataType: "json",
     success: () => {
-      console.log("Deleted Successfully")
+      console.log("Deleted Successfully");
     }
   });
 }
 
-$(document).on('click', ".delete", function(){
+$(document).on("click", ".delete", function() {
   deleteCard($(this).attr("id"));
-  $(this).parent().remove();
+  $(this)
+    .parent()
+    .remove();
 });
 
-$(document).on('click', "#start-button", function(){
-    $(".start-text").hide();
+$(document).on("click", "#start-button", function() {
+  $(".start-text").hide();
+  displayCardQuestion(currentCard);
+});
+
+$(document).on("click", "#answer-button", function() {
+  displayCardAnswer(currentCard);
+});
+
+$(document).on("click", "#next-card", function() {
+  currentCard = getCard(cards_js);
+  if (currentCard) {
     displayCardQuestion(currentCard);
+  } else {
+    displayEnd();
+  }
 });
 
-$(document).on('click', "#answer-button", function(){
-    displayCardAnswer(currentCard);
+$(document).on("submit", "#new-card-form", function(e) {
+  e.preventDefault();
+  var newCard = {};
+
+  newCard.question = $("#question").val();
+  newCard.answer = $("#answer").val();
+  newCard.reference = $("#reference").val();
+  $.ajax({
+    type: "POST",
+    url: "/cards",
+    data: JSON.stringify(newCard),
+    success: () => console.log("Post Success"),
+    dataType: "json",
+    contentType: "application/json"
+  });
+  cards_js.push(newCard);
+  document.getElementById("new-card-form").reset();
 });
 
-$(document).on('click', "#next-card", function(){
-    currentCard = getCard(cards_js);
-    if (currentCard) {
-      displayCardQuestion(currentCard);
-    }
-    else {
-      displayEnd();
-    }
-});
-
-$(document).on('submit', "#new-card-form", function(e){
-    e.preventDefault();
-    var newCard ={};
-
-    newCard.question = $("#question").val();
-    newCard.answer = $("#answer").val();
-    newCard.reference = $("#reference").val();
-
-    console.log(newCard);
-    $.ajax({
-      type: "POST",
-      url: "/cards",
-      data: JSON.stringify(newCard),
-      success: () => console.log("Post Success"),
-      dataType: "json",
-      contentType: "application/json"
-    });
-    cards_js.push(newCard);
-    document.getElementById("new-card-form").reset();
-});
-
-$(document).on('click', "#summary-button", function(){
-  $('#summary-button').remove();
-  $('.summary-title').addClass('hidden');
-  $('.summary-list').removeClass('hidden');
-  $('.cards-summary').removeClass('hidden');
+$(document).on("click", "#summary-button", function() {
+  $("#summary-button").remove();
+  $(".summary-title").addClass("hidden");
+  $(".summary-list").removeClass("hidden");
+  $(".cards-summary").removeClass("hidden");
   displaySummary(cards_js);
 });
+
+// END
