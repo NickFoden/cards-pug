8f0d781be4b6215d922aa9487575feec8e1da613
Nick Foden <nickfoden@gmail.com>
2018-02-18 08:15:20 -0500

    Removed cookie parser

--- routerUsers.js
+++ routerUsers.js
@@ -1,38 +1,41 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const Strategy = require('passport-local').Strategy;
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const Strategy = require("passport-local").Strategy;
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const SECRET_KEY = require("./config.js");
 
 mongoose.Promise = global.Promise;
-const {User} = require('./models.js');
-const cookieParser = require('cookie-parser');
+const { User } = require("./models.js");
+// const cookieParser = require("cookie-parser");
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
-router.use(cookieParser());
-router.use(require('express-session')({
-    secret: 'keyboard cat',
+// router.use(cookieParser());
+router.use(
+  require("express-session")({
+    secret: SECRET_KEY,
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', { 
-    successRedirect: '/summary', 
-    failureRedirect: '/sign-up'
+router.post("/login", function(req, res, next) {
+  passport.authenticate("local", {
+    successRedirect: "/summary",
+    failureRedirect: "/sign-up"
   })(req, res, next);
 });
 
-router.get('/logout',
-  function(req, res){
-    req.logout();
-    res.redirect('index');
-  });
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("index");
+});
 
-module.exports = {router};
+module.exports = { router };
 
 //End
